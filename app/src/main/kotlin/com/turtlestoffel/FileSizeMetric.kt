e835d7ef205d422a4df54f9fffa4e9930a5ed892
TurtleStoffel <me@turtlestoffel.com>
2024-12-20 18:56:19 +0100

    Add extension to output

--- app/src/main/kotlin/com/turtlestoffel/FileSizeMetric.kt
+++ app/src/main/kotlin/com/turtlestoffel/FileSizeMetric.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SizeMetric(
     val path: String,
+    val extension: String,
     val size: Int,
 )
 
@@ -12,7 +13,7 @@ class FileSizeMetric(
     private val codeFiles: List<CodeFile>,
 ) {
     fun calculate(): List<SizeMetric> {
-        return codeFiles.map { SizeMetric(it.path, it.getNumberOfLines()) }
+        return codeFiles.map { SizeMetric(it.path, it.extension, it.getNumberOfLines()) }
     }
 
     fun calculateHistogram() {
