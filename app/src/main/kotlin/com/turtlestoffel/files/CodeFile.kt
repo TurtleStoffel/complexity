a4a3a0c359dbec411f6c89fbab051a8b6f962513
TurtleStoffel <me@turtlestoffel.com>
2025-01-04 15:52:24 +0100

    Account for last line without newline in line count

--- app/src/main/kotlin/com/turtlestoffel/files/CodeFile.kt
+++ app/src/main/kotlin/com/turtlestoffel/files/CodeFile.kt
@@ -2,7 +2,8 @@ package com.turtlestoffel.files
 
 class CodeFile(val repositoryFile: RepositoryFile) {
     val newLines: Int by lazy {
-        repositoryFile.content.count { it == '\n' }
+        // + 1 needed to account for the last line that doesn't end with a newline character
+        repositoryFile.content.count { it == '\n' } + 1
     }
     val numberOfImportStatements: Int by lazy {
         repositoryFile.content.windowed(IMPORT_KEYWORD.length).count { it == IMPORT_KEYWORD }
