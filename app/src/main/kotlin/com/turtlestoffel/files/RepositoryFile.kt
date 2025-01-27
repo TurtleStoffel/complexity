46fccd19f57645bdc301a7a63f46d557e10b075e
TurtleStoffel <me@turtlestoffel.com>
2025-01-27 20:59:25 +0100

    Comment Parsing Imports from AST

--- app/src/main/kotlin/com/turtlestoffel/files/RepositoryFile.kt
+++ app/src/main/kotlin/com/turtlestoffel/files/RepositoryFile.kt
@@ -9,7 +9,7 @@ import kotlin.io.path.extension
  */
 data class RepositoryFile(
     val relativePath: Path,
-    private val absolutePath: Path,
+    val absolutePath: Path,
 ) {
     val extension = relativePath.extension
     val filename: Path = relativePath.fileName
